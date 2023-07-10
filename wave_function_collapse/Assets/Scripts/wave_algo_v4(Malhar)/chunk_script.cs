60a24003b6d11b6effc12ee094f13b0fecde6f2e
Harsh Jain <harshjain17may@gmail.com>
2023-07-10 17:29:00 +0530

    build is running with wall run

--- wave_function_collapse/Assets/Scripts/wave_algo_v4(Malhar)/chunk_script.cs
+++ wave_function_collapse/Assets/Scripts/wave_algo_v4(Malhar)/chunk_script.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class chunk_script : MonoBehaviour
